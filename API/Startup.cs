ad415f9933f95a8d5274f7c1d6902b308b299df8
mbisovskyi <mbisovskyi@gmail.com>
2022-11-22 20:04:52 -0500

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -27,6 +27,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddDbContext<DataContext>(options => {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
@@ -35,6 +36,8 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
+            
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,8 +58,10 @@ namespace API
 
             app.UseEndpoints(endpoints =>
             {
+                app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
                 endpoints.MapControllers();
             });
+            
         }
     }
 }
